60859e67f596c99511910a754df35e671dc32050
Connor Dickson <68393151+cdcksn@users.noreply.github.com>
2025-05-06 04:50:00 +0000

    06_03_E

--- sql_practice.sql
+++ sql_practice.sql
@@ -1,5 +1,36 @@
-select OrderDate
+select EmpID
+  , OrderDate
   , OrderTotal
 from red_30_tech_us_sales_cleaned
-order by OrderTotal desc
-limit 10 
+where EmpID = 900015476
+order by OrderDate
+
+
+select EmpID
+  , OrderDate
+  , OrderTotal
+from red_30_tech_us_sales_cleaned
+where EmpID = 900012972
+order by OrderDate
+
+
+select EmpID
+  , OrderDate
+  , OrderTotal
+from red_30_tech_us_sales_cleaned
+where EmpID = 900010875
+order by OrderDate
+
+
+select EmpID
+  , OrderDate
+  , OrderTotal
+from red_30_tech_us_sales_cleaned
+where EmpID in (900015476, 900012972, 900010875)
+order by OrderDate
+
+
+select * 
+from red_30_tech_us_sales_cleaned
+where OrderType = 'Retail'
+  and OrderDate between '2018-08-01' and '2018-08-31'
