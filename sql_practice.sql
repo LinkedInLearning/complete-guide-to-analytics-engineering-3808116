aaf483db9ca501cc9b2838efad9825c3a2464db7
Connor Dickson <68393151+cdcksn@users.noreply.github.com>
2025-07-15 22:47:28 +0000

    07_05_E

--- sql_practice.sql
+++ sql_practice.sql
@@ -142,3 +142,26 @@ select sr.State
 from red_30_tech_us_sales_regions as sr 
 full join us_state_codes as usc 
   on sr.State = usc.state_name
+
+
+select EmpID
+  , max(julianday(OrderDate)) - min(julianday(OrderDate)) as MinMaxDateDiffDays
+from red_30_tech_us_sales_cleaned
+where EmpID = 900012972
+
+
+
+select EmpID
+  , max(julianday(OrderDate)) - min(julianday(OrderDate)) as MinMaxDateDiffDays
+from red_30_tech_us_sales_cleaned
+where EmpID = 900012972
+
+
+{% macro min_max_date_macro(field_name) %}
+    max(julianday({{field_name}}) - min(julianday({{field_name}})
+{% endmarco %}
+
+select EmpID
+  , {{ min_max_date_macro('OrderDate') }} as MinMaxDateDiffDays
+from red_30_tech_us_sales_cleaned
+where EmpID = 900012972
